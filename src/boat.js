9910f18c0eb07dbee28d8f5a50b3f8ff1a77c514
blatzwave <aqcfernandes@gmail.com>
2020-05-24 20:14:39 +0100

    Display/update boat data on webpage.

--- src/boat.js
+++ src/boat.js
@@ -3,11 +3,17 @@ class Boat {
         this._lat = lat; // latitude
         this._lon = lon; // longitude
         this._hdg = hdg; // heading
-        this._bsp = 0;   // boat speed
+        this._bsp = 10;   // boat speed
     }
 
     currPos() {
         console.log("Latitude: " + this._lat + ", Longitude: " + this._lon);
+    }
+
+    distTravelled() {
+        metricBSP = this._bsp * 0.514;
+        dist = metricBSP * 30;
+        return dist;
     }
 
     get lat() {
@@ -18,13 +24,21 @@ class Boat {
         return this._lon;
     }
 
+    get hdg() {
+        return this._hdg;
+    }
+
+    get bsp() {
+        return this._bsp;
+    }
+
     set hdg(hdg) {
         this._hdg = hdg;
     }
     
-    calcBSP() {
-        this._bsp = 7;
-    }
+    
+
+
 }
 
 module.exports = Boat;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Boat = require('./boat.js');
-
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
@@ -8,7 +8,20 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 boat = new Boat(0,0,0);
-boat.currPos();
+
+
+
+
+
+app.get('/api', (request, response) => {
+    console.log("Sending boat data...");
+    response.json({
+        lat: boat.lat,
+        lon: boat.lon,
+        bsp: boat.bsp,
+        hdg: boat.hdg
+    })
+})
 
 app.post('/api', (request, response) => {
     console.log("I got a request!");
@@ -17,6 +30,8 @@ app.post('/api', (request, response) => {
         status: 'success',
         newHDG: request.body.newHDG
     });
+    boat.hdg = request.body.newHDG;
+    console.log("Confirmed boat heading: " + boat.hdg);
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
