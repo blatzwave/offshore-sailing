7b7b189f91243df85773d51c661facb89b6ab7c5
blatzwave <aqcfernandes@gmail.com>
2020-05-29 16:55:21 +0100

    fixed broken API key

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.listen(port, () =>
 async function getWind(lat, lon) {
   console.log(`About to fetch wind data for pos: ${lat} , ${lon}`);
   const APIkey = process.env.API_KEY;
-  //const APIkey = "cfb3d4d2a22bb55307dd74278746b25";
+
   let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`);
   let data = await response.json();
   return data;
